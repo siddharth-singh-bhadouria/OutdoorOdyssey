77c8a3f161adddfeaf8552e05e30cb71486ad16e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-25 02:12:26 +0530

    fix

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.use((err, req, res, next) => {
   res.status(status).render("error", { err });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`LISTENING TO PORT ${port}!!`);
