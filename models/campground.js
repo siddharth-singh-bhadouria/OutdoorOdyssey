f20c88a954ac80ccd503273da8b25b0745af090d
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-24 20:10:15 +0530

    The completed project from Web Development Learning repository

---
+++ models/campground.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+const Review = require('./review')
+const Schema = mongoose.Schema
+
+const imageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+imageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200')
+})
+
+const opts = { toJSON: { virtuals: true } }
+
+const campgroundSchema = new Schema({
+    title: String,
+    images: [imageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    price: Number,
+    description: String,
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+}, opts)
+
+campgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    try {
+        return `<strong><a href=campgrounds/${this._id}>${this.title}</a></strong>`
+    }
+    catch (e) {
+        console.log('JSON fucking died1', e)
+    }
+})
+
+campgroundSchema.post('findOneAndDelete', async (data) => {
+    if (data) {
+        await Review.deleteMany({ _id: { $in: data.reviews } })
+    }
+})
+
+const Campground = new mongoose.model('Campground', campgroundSchema)
+
+module.exports = Campground
+
